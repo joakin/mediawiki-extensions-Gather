1213e2c4475af1b844b52e45da5be9c689fcdcbc
jdlrobson <jdlrobson@gmail.com>
2015-03-25 16:44:21 -0700

    QA: Browser test for edit workflow
    This will ensure edit doesnt break again.
    
    Bug: T93949
    Change-Id: If7a83c73c109c7508086229c65325a10f7f6ae30

--- tests/browser/features/step_definitions/edit_collection_steps.rb
+++ tests/browser/features/step_definitions/edit_collection_steps.rb
@@ -9,3 +9,19 @@ end
 Then(/^I see the collection editor overlay$/) do
   expect(on(GatherPage).edit_overlay_element.when_present).to be_visible
 end
+
+Then(/^I enter "(.*?)" as the description$/) do |keys|
+  on(GatherPage).edit_overlay_description_element.when_present.send_keys(keys)
+end
+
+Then(/^I click done$/) do
+  on(GatherPage).edit_overlay_done_element.when_present.click
+end
+
+Then(/^the page has reloaded$/) do
+  sleep 2
+end
+
+Then(/^the description of my collection is "(.*?)"$/) do |text|
+  expect(on(GatherPage).collection_description_element.when_present.text).to match text
+end
