24d6c7060c6c9ef00f4280bc66c5c021c30a0a29
Yuri Astrakhan <yurik@wikimedia.org>
2015-03-26 13:45:34 -0400

    Removed table autodrop if you have older schema
    We can't go into production with a potential of
    droping tables. (e.g. someone accidently adds that index later)
    
    Make sure you have ran update.php on your machine
    before this patch gets merged.
    
    Change-Id: I31738f23c4d68a3e2453833a5596993af54d3405

--- schema/Updater.hooks.php
+++ schema/Updater.hooks.php
@@ -11,13 +11,6 @@ class UpdaterHooks {
 	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $du ) {
 		$dir = __DIR__;
 
-		// TODO BUG !!!
-		// Remove this before going to production - good enough migration for BETA & DEV
-		if ( $du->getDB()->indexExists( 'gather_list_item', 'gli_id_order_ns_title', __METHOD__ ) ) {
-			$du->dropExtensionTable( 'gather_list_item', false );
-			$du->dropExtensionTable( 'gather_list', false );
-		}
-
 		$du->addExtensionTable( 'gather_list', "$dir/gather_list.sql" );
 		$du->addExtensionTable( 'gather_list_item', "$dir/gather_list_item.sql" );
 
