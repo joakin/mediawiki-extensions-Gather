e3828c6071aa4284ac85c93d4844303f8fde0bfc
joakin <joaquin@chimeces.com>
2015-05-08 01:47:42 +0200

    Fix exception if image passed to models is a boolean
    Some "false" propagates to the $collection->file and it ends up exploding here
    in this if... Probably coming from the api.
    
    Bug: T98545
    Change-Id: I9edf368f033ee762e2be32ebed9a7e01f6e13501

--- includes/models/Image.php
+++ includes/models/Image.php
@@ -14,11 +14,12 @@ class Image {
 	const SHARING_THUMBNAIL_WIDTH = 360;
 
 	/**
+	 * @param File $image
 	 * @param int $size
 	 * @return bool|\MediaTransformOutput
 	 */
 	public static function getThumbnail( $image, $size = self::SHARING_THUMBNAIL_WIDTH ) {
-		if ( $image !== null ) {
+		if ( $image ) {
 			$thumb = $image->transform( array( 'width' => $size ) );
 			if ( $thumb && $thumb->getUrl() ) {
 				return $thumb;
