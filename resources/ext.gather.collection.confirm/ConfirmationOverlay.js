d8fff3d52a873d48ee86a1f68db3049b1216b208
jdlrobson <jdlrobson@gmail.com>
2015-05-12 16:51:34 +0100

    Fix confirmation message styling inside overlay
    * Drop overlay class which makes it have height 100%;
    * give z-index above overlay
    
    Bug: T98538
    Change-Id: I9dc8ed5d2f5c482a348bf10c106bb7ff0303e27b

--- resources/ext.gather.collection.confirm/ConfirmationOverlay.js
+++ resources/ext.gather.collection.confirm/ConfirmationOverlay.js
@@ -12,7 +12,7 @@
 	 */
 	ConfirmationOverlay = CollectionsContentOverlayBase.extend( {
 		/** @inheritdoc */
-		className: 'overlay collection-confirmation-overlay content-overlay position-fixed',
+		className: 'collection-confirmation-overlay content-overlay position-fixed',
 		/** @inheritdoc */
 		defaults: $.extend( {}, CollectionsContentOverlayBase.prototype.defaults, {
 			fixedHeader: false,
