3ff3fd9b457d381f1864b4cb7485daeb57b4af54
jdlrobson <jdlrobson@gmail.com>
2015-06-03 18:38:03 -0700

    QA: Fix create collection selector
    Change-Id: Ia546c4ad1593f716f72238a06abca029fcb11bc1

--- tests/browser/features/support/pages/article_page.rb
+++ tests/browser/features/support/pages/article_page.rb
@@ -17,5 +17,5 @@ class ArticlePage
   div(:collections_overlay, css: '.collection-overlay')
   li(:collections_overlay_collection_one, css: '.collection-overlay ul li', index: 0)
   text_field(:collection_overlay_new_collection_input, css: '.collection-overlay input')
-  button(:collection_overlay_new_collection_button, css: '.collection-overlay .oo-ui-buttonElement-button')
+  button(:collection_overlay_new_collection_button, css: '.collection-overlay .create-collection')
 end
