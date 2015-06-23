83b9f61eec2f7a61e6deac1d1a72a8abb2f9b230
jdlrobson <jdlrobson@gmail.com>
2015-06-23 13:19:45 -0700

    Introduce tests for pagination
    Introduce tests for recent collections pagination
    
    Bug: T103570
    Change-Id: I0ff19951eeb5f97e1e6a35fc7306d222fc0270e1

--- tests/browser/features/support/pages/gather_recent_page.rb
+++ tests/browser/features/support/pages/gather_recent_page.rb
@@ -3,4 +3,8 @@ class GatherRecentPage
 
   page_url 'Special:Gather/all/recent'
   a(:my_collections_button, css: '.button-bar a', index: 1)
+  div(:collection_one_hundred, css: '.collection-card', index: 99 )
+  div(:collection_one_hundred_plus_one, css: '.collection-card', index: 100 )
+  div(:collection_one_hundred_plus_one_title, css: '.collection-card .collection-card-title', index: 100 )
+  a(:collection_one_hundred_plus_one_owner, css: '.collection-card .collection-owner', index: 100 )
 end
