94941e2e36e90741d5048979f5e9c345a4e77d65
joakin <joaquin@chimeces.com>
2015-08-24 14:50:51 +0200

    Fix lint warnings in webpack.config.js
    * Format the file with mediawiki style.
    
    Change-Id: I2af92c391b035319c8bcc827beeaed00a71f8dcf

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,46 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var UpdateResourceLoaderConfigPlugin = require('update-resourceloader-config-plugin');
+var path = require( 'path' ),
+	webpack = require( 'webpack' ),
+	ExtractTextPlugin = require( 'extract-text-webpack-plugin' ),
+	UpdateResourceLoaderConfigPlugin = require( 'update-resourceloader-config-plugin' ),
+	isProduction = process.env.NODE_ENV === 'production',
+	out = 'build/',
+	conf = null;
 
-var isProduction = process.env.NODE_ENV === 'production';
-
-var out = 'build/';
-
-var conf = {
+conf = {
 	entry: {
 		'resources/ext.gather.special.collection/init': './resources/ext.gather.special.collection/init.js'
 	},
 	output: {
-		path: path.join(__dirname, out),
+		path: path.join( __dirname, out ),
 		filename: '[name].js'
 	},
 	module: {
-		loaders: [
-			{ test: /\.(gif|png|jpg)$/, loader: 'url?limit=25000' },
-			{ test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!less') },
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-		]
+		loaders: [ {
+			test: /\.(gif|png|jpg)$/,
+			loader: 'url?limit=25000'
+		}, {
+			test: /\.less$/,
+			loader: ExtractTextPlugin.extract( 'style', 'css!autoprefixer!less' )
+		}, {
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		} ]
 	},
 	plugins: [
 		// new webpack.NoErrorsPlugin(),
-		new ExtractTextPlugin('style.css', { allChunks: true }),
-		// new webpack.optimize.DedupePlugin(),
-		new UpdateResourceLoaderConfigPlugin({
+		new ExtractTextPlugin( 'style.css', {
+			allChunks: true
+		} ),
+		new webpack.optimize.DedupePlugin(),
+		new UpdateResourceLoaderConfigPlugin( {
 			i18n: 'mw.msg'
-		})
+		} )
 	]
 };
 
-if (!isProduction) {
-	conf.devtool = "#inline-source-map";
+if ( !isProduction ) {
+	conf.devtool = '#inline-source-map';
 }
 
 module.exports = conf;
