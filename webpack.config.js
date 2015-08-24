b128ac535c3696a7f1a3004bdbc52ea33def6de1
joakin <joaquin@chimeces.com>
2015-08-24 12:41:21 +0200

    Webpack indentation to tabs as in the project
    Change-Id: Ic1692c7e71d5bf72ef85537a1dd43259e12d9452

--- webpack.config.js
+++ webpack.config.js
@@ -9,36 +9,36 @@ var isProduction = process.env.NODE_ENV === 'production';
 var out = 'build/';
 
 var conf = {
-  entry: {
-    'resources/ext.gather.special.collection/init': './resources/ext.gather.special.collection/init.js'
-  },
-  output: {
-    path: path.join(__dirname, out),
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [
-      { test: /\.(gif|png|jpg)$/, loader: 'url?limit=25000' },
-      { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!less') },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ]
-  },
-  plugins: [
-    // new webpack.NoErrorsPlugin(),
-    new ExtractTextPlugin('style.css', { allChunks: true }),
-    // new webpack.optimize.DedupePlugin(),
-    new ExtractI18nKeys({
+	entry: {
+		'resources/ext.gather.special.collection/init': './resources/ext.gather.special.collection/init.js'
+	},
+	output: {
+		path: path.join(__dirname, out),
+		filename: '[name].js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.(gif|png|jpg)$/, loader: 'url?limit=25000' },
+			{ test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer!less') },
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+		]
+	},
+	plugins: [
+		// new webpack.NoErrorsPlugin(),
+		new ExtractTextPlugin('style.css', { allChunks: true }),
+		// new webpack.optimize.DedupePlugin(),
+		new ExtractI18nKeys({
 			functionName: 'mw.msg',
 			onKeys: console.log.bind(null, '\n\ni18n keys:\n')
 		}),
-    new ExtractMFRequiredModules({
+		new ExtractMFRequiredModules({
 			onModules: console.log.bind(null, '\n\nModules:\n')
 		})
-  ]
+	]
 };
 
 if (!isProduction) {
-  conf.devtool = "#inline-source-map";
+	conf.devtool = "#inline-source-map";
 }
 
 module.exports = conf;
